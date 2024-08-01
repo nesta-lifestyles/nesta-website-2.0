3bcc64931e9ad3ec66c14edbc0ebb57898ee0c87
Mahantesha <mahantesha@Mahanteshas-Laptop.local>
2024-08-01 20:10:42 +0530

    fixing mobile view for nesta design

--- src/layouts/process_layout/index.js
+++ src/layouts/process_layout/index.js
@@ -2,20 +2,32 @@ import React from "react"
 import styled from "styled-components"
 import firstTestImage from"../../images/designs/process.png"
 import { Image } from "../../elements/image"
+import processImage from "../../images/mobile/process_2.png"
+import { BrowserView, isMobile, MobileView } from "react-device-detect"
 
 const ProcessContainer = styled.div`
 `
+
+const getImage=()=>{
+    console.log("Is Mobile"+isMobile) 
+    if(isMobile) {
+        return processImage
+    }
+    return firstTestImage
+}
 
 
 export const ProcessLayout = () => {
     return(<>
     <ProcessContainer> 
-        <Image  width= "1206.11px"
+        <Image  
+        width= "100%"
         height= "856px"
-        src={firstTestImage}
+        src={getImage()}
         alt="slider image"
         id="first_image"
         />
+       
         </ProcessContainer>
  
     </>)
--- src/pages/nesta_design/nesta_design.js
+++ src/pages/nesta_design/nesta_design.js
@@ -5,6 +5,8 @@ import { NestaDesignInformation } from "../../components/navbar/nav_bar_content"
 import { GalleryLayout } from "../../layouts/gallery_layout";
 import { BannerLayout } from "../../layouts/banner_layout";
 import { NestaDesignHomePageData } from "./data/home";
+import { ProcessLayout } from "../../layouts/process_layout";
+import MeetAnExpert from "../../components/meet_an_expert";
 
 const Container = styled.div`
     /* margin-left: 70px;
@@ -36,9 +38,9 @@ export const NestaDesignHomePage = () => {
             <Container>
                     <BannerLayout title={NestaDesignHomePageData.banner.banner_title} src={NestaDesignHomePageData.banner.image} animationflag ={true} OnPrevIndicatorClick={HandlePreviousDesign} onNextIndicatorClick={HandleNextDesign}/>
                     <ExploreModelsLayout models={NestaDesignInformation}/>
-                    {/* <ProcessLayout/> */}
+                    <ProcessLayout/>
                    <GalleryLayout images={NestaDesignHomePageData.gallery}></GalleryLayout>
-                   {/* <MeetAnExpert/> */}
+                   <MeetAnExpert/>
             </Container>
         </>)
 
