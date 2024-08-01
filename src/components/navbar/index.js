46add2a4f35deaa3a41f528e3680384e1e52a481
Mahantesha <mahantesha@Mahanteshas-Laptop.local>
2024-08-02 02:30:51 +0530

    fixing menu for mobile view

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -220,38 +220,6 @@ const CloseIO = styled.div`
    } */
 `
 
-const Button = styled.div`
-    position: absolute;
-    right: 5px;
-    top: 5px;
-    border-radius: 50%;
-    color: black;
-    width: 34px;
-    padding: 5px;
-    height: 34px;
-    font: 400 14px/150% Poppins, sans-serif;
-    background-color: white;
-    transition: transform 0.1s, box-shadow 0.1s;
-    cursor: pointer;
-    @media (max-width: 991px) {
-        padding: 0 20px;
-    }
-
-    &:hover{
-        transition: 0.35s;
-        /* transform: scale(1.05); */
-        font-weight: 500;
-        text-decoration: dashed;
-        opacity: 1.0;
-        background-color: white;
-        box-shadow: 10px 10px 20px rgba(36, 36, 36, 0.5); 
-    }
-    
-    &:active{
-        transform: scale(0.95);
-        box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
-    }
-`
 
 
 const Navbar=() => {
